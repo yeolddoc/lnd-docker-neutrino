ad87e6b9257b094a5b35002ed6cb7e65eb07d5b3
yeolddoc <40762594+yeolddoc@users.noreply.github.com>
2018-09-28 01:02:52 +0200

    Expose 10009 + 9735

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo "Creating Docker image"
 docker build -t lnd-docker-neutrino .
 
 echo "Running Docker container"
-docker run -td --name lnd lnd-docker-neutrino
+docker run -td -p 10009:10009 -p 9735:9735 --name lnd lnd-docker-neutrino
 
 echo "Giving Docker container some time..."
 sleep 120
