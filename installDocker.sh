ccd04d6ed0f5d160ee17aa804c5586c3ba618fda
yeolddoc <40762594+yeolddoc@users.noreply.github.com>
2018-09-28 00:27:04 +0200

    Create installDocker.sh

---
+++ installDocker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt -y update
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt -y update
+sudo apt -y install docker-ce
