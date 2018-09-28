bc7f18f58cc2cbb8159f3a8caed13775bf57e161
YeOldDoc <40762594+yeolddoc@users.noreply.github.com>
2018-09-28 07:30:49 +0200

    Extract tls.cert  + admin.macaroon

--- setup.sh
+++ setup.sh
@@ -8,10 +8,18 @@ docker run -td -p 10009:10009 -p 9735:9735 --name lnd lnd-docker-neutrino
 echo "Giving Docker container some time..."
 sleep 120
 
+mkdir data
+
+echo "Retriving certificate"
+docker cp lnd:/root/.lnd/tls.cert ./data/
+
+echo "Retriving admin macaroon"
+docker cp lnd:/root/.lnd/data/chain/bitcoin/testnet/admin.macaroon ./data/
+
 echo "Getting and saving public key"
-docker exec -t lnd lncli -n testnet getinfo > getinfo.json
+docker exec -t lnd lncli -n testnet getinfo > ./data/getinfo.json
 
 echo "Creating new wallet address"
-docker exec -t lnd lncli -n testnet newaddress np2wkh > address.json
+docker exec -t lnd lncli -n testnet newaddress np2wkh > ./data/address.json
 
 echo "Finished"
